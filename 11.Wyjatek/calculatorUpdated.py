ef26d998a94d048b8e4b7643a437929a3ece1ce2
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-05-16 09:05:42 +0200

    Updated

---
+++ 11.Wyjatek/calculatorUpdated.py
@@ -0,0 +1,63 @@
+def dodawanie(a, b):
+    return a + b
+
+def odejmowanie(a, b):
+    return a - b
+
+def mnozenie(a, b):
+    return a * b
+
+
+def dzielenie(a, b):
+    try:
+        if b == 0:
+            raise ZeroDivisionError("Nie można dzielić przez zero!")
+        else:
+            return a / b
+    except ZeroDivisionError as e:
+        print(e)
+        return None
+
+def menu():
+    print("Wybierz operację:")
+    print("1 - Dodawanie")
+    print("2 - Odejmowanie")
+    print("3 - Mnożenie")
+    print("4 - Dzielenie")
+    print("q - Wyjście")
+
+def pobierz_liczby():
+    a = float(input("Podaj pierwszą liczbę: "))
+    b = float(input("Podaj drugą liczbę: "))
+    return a, b
+
+while True:
+    menu()
+    wybor = input("Twój wybór: ")
+
+    if wybor == "q":
+        print("Koniec programu.")
+        break
+
+    if wybor in ["1", "2", "3", "4"]:
+        a, b = pobierz_liczby()
+
+        if wybor == "1":
+            wynik = dodawanie(a, b)
+            print("Wynik dodawania:", wynik)
+
+        elif wybor == "2":
+            wynik = odejmowanie(a, b)
+            print("Wynik odejmowania:", wynik)
+
+        elif wybor == "3":
+            wynik = mnozenie(a, b)
+            print("Wynik mnożenia:", wynik)
+
+        elif wybor == "4":
+            wynik = dzielenie(a, b)
+            if wynik is not None:
+                print("Wynik dzielenia:", wynik)
+
+    else:
+        print("Nieprawidłowy wybór.")
