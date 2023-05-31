35463727f57011d8f531feea90b2cde3e7d0c9fd
Vladyslav Maksymov <vmaksymovk@gmail.com>
2023-05-31 23:45:40 +0200

    Fixed ID

--- ToDoList/todo.py
+++ ToDoList/todo.py
@@ -2,65 +2,92 @@ import json
 
 class TaskManager:
     def __init__(self):
-        self.tasks = []
+        self.tasks = {}
+        self.next_id = 1
         self.load_tasks_from_file()
 
     def load_tasks_from_file(self):
         try:
-            with open('tasks.json', 'r') as file:
-                self.tasks = json.load(file)
+            with open('tasks.json', 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                self.tasks = data['tasks']
+                self.next_id = data['next_id']
         except FileNotFoundError:
-            self.tasks = []
+            self.tasks = {}
+            self.next_id = 1
 
     def save_tasks_to_file(self):
-        with open('tasks.json', 'w') as file:
-            json.dump(self.tasks, file, indent=4)
+        data = {
+            'tasks': self.tasks,
+            'next_id': self.next_id
+        }
+        try:
+            with open('tasks.json', 'w', encoding='utf-8') as file:
+                json.dump(data, file, indent=4)
+            print("Tasks saved to file successfully!")
+        except Exception as e:
+            print("An error occurred while saving tasks to file:", str(e))
 
     def add_task(self):
-        title = input("Enter task title: ")
-        description = input("Enter task description: ")
-        deadline = input("Enter task deadline: ")
+        try:
+            title = input("Enter task title: ")
+            description = input("Enter task description: ")
+            deadline = input("Enter task deadline: ")
 
-        task = {
-            'id': len(self.tasks) + 1,
-            'title': title,
-            'description': description,
-            'deadline': deadline
-        }
-        self.tasks.append(task)
-        self.save_tasks_to_file()
-        print("Task added successfully!")
+            task = {
+                'title': title,
+                'description': description,
+                'deadline': deadline,
+                'description_shown': False  
+            }
+            self.tasks[self.next_id] = task
+            self.next_id += 1
+            self.save_tasks_to_file()
+            print("Task added successfully!")
+        except Exception as e:
+            print("An error occurred while adding the task:", str(e))
 
-    def display_task(self, task):
-        print("ID:", task['id'])
+    def display_task(self, task_id):
+        task = self.tasks[task_id]
+        print("ID:", task_id)
         print("Title:", task['title'])
         print("Deadline:", task['deadline'])
-        show_description = input("Show description? (y/n): ")
-        if show_description.lower() == 'y':
-            print("Description:", task['description'])
+        if not task['description_shown']:  
+            show_description = input("Show description? (y/n): ")
+            if show_description.lower() == 'y':
+                print("Description:", task['description'])
+                task['description_shown'] = True  
 
     def view_tasks(self):
-        if not self.tasks:
-            print("No tasks found.")
-        else:
-            for task in self.tasks:
-                self.display_task(task)
-                print()
+        try:
+            if not self.tasks:
+                print("No tasks found.")
+            else:
+                for task_id in self.tasks:
+                    self.display_task(task_id)
+                    print()
+        except Exception as e:
+            print("An error occurred while viewing tasks:", str(e))
 
     def delete_task(self):
-        task_id = int(input("Enter the ID of the task to delete: "))
-        for task in self.tasks:
-            if task['id'] == task_id:
-                self.tasks.remove(task)
+        try:
+            task_id = int(input("Enter the ID of the task to delete: "))
+            if task_id in self.tasks:
+                del self.tasks[task_id]
                 self.save_tasks_to_file()
                 print("Task deleted successfully!")
-                return
-        print("Task not found.")
+            else:
+                print("Task not found.")
+        except ValueError:
+            print("Invalid task ID. Please enter a valid number.")
+        except Exception as e:
+            print("An error occurred while deleting the task:", str(e))
 
     def update_task(self):
-        task_id = int(input("Enter the ID of the task to update: "))
-        for task in self.tasks:
-            if task['id'] == task_id:
+        try:
+            task_id = int(input("Enter the ID of the task to update: "))
+            if task_id in self.tasks:
+                task = self.tasks[task_id]
                 print("Enter new task details (leave empty to keep the existing value):")
                 title = input("Enter task title: ")
                 description = input("Enter task description: ")
@@ -75,42 +102,53 @@ class TaskManager:
 
                 self.save_tasks_to_file()
                 print("Task updated successfully!")
-                return
-        print("Task not found.")
+            else:
+                print("Task not found.")
+        except ValueError:
+            print("Invalid task ID. Please enter a valid number.")
+        except Exception as e:
+            print("An error occurred while updating the task:", str(e))
 
     def manual_save_to_file(self):
-        self.save_tasks_to_file()
-        print("Tasks saved to file successfully!")
+        try:
+            self.save_tasks_to_file()
+        except Exception as e:
+            print("An error occurred while manually saving tasks to file:", str(e))
 
     def run(self):
-        while True:
-            print("\nTask Manager")
-            print("1. Add Task")
-            print("2. View Tasks")
-            print("3. Delete Task")
-            print("4. Update Task")
-            print("5. Manual Save to File")
-            print("6. Exit")
+        try:
+            self.view_tasks() 
 
-            choice = input("Enter your choice (1-6): ")
-            print()
+            while True:
+                print("\nTask Manager")
+                print("1. Add Task")
+                print("2. View Tasks")
+                print("3. Delete Task")
+                print("4. Update Task")
+                print("5. Manual Save to File")
+                print("6. Exit")
 
-            if choice == '1':
-                self.add_task()
-            elif choice == '2':
-                self.view_tasks()
-            elif choice == '3':
-                self.delete_task()
-            elif choice == '4':
-                self.update_task()
-            elif choice == '5':
-                self.manual_save_to_file()
-            elif choice == '6':
-                self.save_tasks_to_file()
-                print("Exiting...")
-                break
-            else:
-                print("Invalid choice. Please try again.")
+                choice = input("Enter your choice (1-6): ")
+                print()
+
+                if choice == '1':
+                    self.add_task()
+                elif choice == '2':
+                    self.view_tasks()
+                elif choice == '3':
+                    self.delete_task()
+                elif choice == '4':
+                    self.update_task()
+                elif choice == '5':
+                    self.manual_save_to_file()
+                elif choice == '6':
+                    self.save_tasks_to_file()
+                    print("Exiting...")
+                    break
+                else:
+                    print("Invalid choice. Please try again.")
+        except Exception as e:
+            print("An error occurred while running the task manager:", str(e))
 
 
 if __name__ == '__main__':
